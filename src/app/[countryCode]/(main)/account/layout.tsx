6469476bdb235c1cf262c158e21c5037f484998c
zeptak <33266931+zeptak@users.noreply.github.com>
2026-04-22 12:20:32 +0200

    Initialize fix/stripe-wrapper

---
+++ src/app/[countryCode]/(main)/account/layout.tsx
@@ -0,0 +1,18 @@
+import { retrieveCustomer } from "@lib/data/customer"
+import AccountLayout from "@modules/account/templates/account-layout"
+
+export default async function AccountPageLayout({
+  dashboard,
+  login,
+}: {
+  dashboard?: React.ReactNode
+  login?: React.ReactNode
+}) {
+  const customer = await retrieveCustomer().catch(() => null)
+
+  return (
+    <AccountLayout customer={customer}>
+      {customer ? dashboard : login}
+    </AccountLayout>
+  )
+}
