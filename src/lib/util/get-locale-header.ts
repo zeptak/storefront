bf4af4fc3baac28db19e9b42dfb77536c2f7b963
zeptak <33266931+zeptak@users.noreply.github.com>
2026-04-22 13:55:23 +0200

    Delete src directory

--- src/lib/util/get-locale-header.ts
+++
@@ -1,8 +0,0 @@
-import { getLocale } from "@lib/data/locale-actions"
-
-export async function getLocaleHeader() {
-  const locale = await getLocale()
-  return {
-    "x-medusa-locale": locale,
-  } as const
-}
