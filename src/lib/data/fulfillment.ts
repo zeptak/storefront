da8c4bf069d75d615d570a55128c0336d897580a
zeptak <33266931+zeptak@users.noreply.github.com>
2026-04-22 12:20:34 +0200

    Initialize feat/free-shipping

---
+++ src/lib/data/fulfillment.ts
@@ -0,0 +1,30 @@
+"use server"
+
+import { sdk } from "@lib/config"
+import { HttpTypes } from "@medusajs/types"
+import { getAuthHeaders, getCacheOptions } from "./cookies"
+
+export const listCartShippingMethods = async (cartId: string) => {
+  const headers = {
+    ...(await getAuthHeaders()),
+  }
+
+  const next = {
+    ...(await getCacheOptions("fulfillment")),
+  }
+
+  return sdk.client
+    .fetch<HttpTypes.StoreShippingOptionListResponse>(
+      `/store/shipping-options`,
+      {
+        method: "GET",
+        query: { cart_id: cartId },
+        headers,
+        next,
+      }
+    )
+    .then(({ shipping_options }) => shipping_options)
+    .catch(() => {
+      return null
+    })
+}
