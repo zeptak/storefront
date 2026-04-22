fcf92226cba32a982f6be77b072bc5b7214e7567
zeptak <33266931+zeptak@users.noreply.github.com>
2026-04-22 12:20:24 +0200

    Initialize fix/price-type

---
+++ src/app/[countryCode]/(checkout)/checkout/page.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from "next"
+import { notFound } from "next/navigation"
+
+import Wrapper from "@modules/checkout/components/payment-wrapper"
+import CheckoutForm from "@modules/checkout/templates/checkout-form"
+import CheckoutSummary from "@modules/checkout/templates/checkout-summary"
+import { enrichLineItems, retrieveCart } from "@lib/data/cart"
+import { HttpTypes } from "@medusajs/types"
+
+export const metadata: Metadata = {
+  title: "Checkout",
+}
+
+const fetchCart = async () => {
+  const cart = await retrieveCart()
+  if (!cart) {
+    return notFound()
+  }
+
+  if (cart?.items?.length) {
+    const enrichedItems = await enrichLineItems(
+      cart?.items,
+      cart?.currency_code
+    )
+    cart.items = enrichedItems as HttpTypes.StoreCartLineItem[]
+  }
+
+  return cart
+}
+
+export default async function Checkout() {
+  const cart = await fetchCart()
+
+  if (!cart) {
+    return notFound()
+  }
+
+  return (
+    <div className="grid grid-cols-1 small:grid-cols-[1fr_416px] content-container gap-x-40 py-12">
+      <Wrapper cart={cart}>
+        <CheckoutForm />
+      </Wrapper>
+      <CheckoutSummary cart={cart} />
+    </div>
+  )
+}
