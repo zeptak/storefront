59413ccbef9856287898dd4ae75e900876527cf3
zeptak <33266931+zeptak@users.noreply.github.com>
2026-04-22 12:19:38 +0200

    Initialize feat/app-router

---
+++ src/modules/layout/templates/footer/index.tsx
@@ -0,0 +1,16 @@
+"use client"
+import FooterCTA from "@modules/layout/components/footer-cta"
+import FooterNav from "@modules/layout/components/footer-nav"
+import MedusaCTA from "@modules/layout/components/medusa-cta"
+
+const Footer = () => {
+  return (
+    <footer>
+      <FooterCTA />
+      <FooterNav />
+      <MedusaCTA />
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/modules/products/templates/index.tsx
@@ -0,0 +1,37 @@
+import { ProductProvider } from "@lib/context/product-context"
+import ProductTabs from "@modules/products/components/product-tabs"
+import RelatedProducts from "@modules/products/components/related-products"
+import ProductInfo from "@modules/products/templates/product-info"
+import React from "react"
+import ImageGallery from "../components/image-gallary"
+import MobileActions from "../components/mobile-actions"
+import { PricedProduct } from "@medusajs/medusa/dist/types/pricing"
+
+type ProductTemplateProps = {
+  product: PricedProduct
+}
+
+const ProductTemplate: React.FC<ProductTemplateProps> = ({ product }) => {
+  return (
+    <>
+      <div className="content-container flex flex-col small:flex-row small:items-start py-6 relative">
+        <div className="flex flex-col gap-y-8 w-full">
+          <ImageGallery images={product?.images || []} />
+        </div>
+        <div
+          id="js-product-section"
+          className="small:sticky small:top-20 w-full py-8 small:py-0 small:max-w-[344px] medium:max-w-[400px] flex flex-col gap-y-12"
+        >
+          <ProductInfo product={product} />
+          <ProductTabs product={product} />
+        </div>
+      </div>
+      <div className="content-container my-16 px-6 small:px-8 small:my-32">
+        <RelatedProducts product={product} />
+      </div>
+      <MobileActions product={product} />
+    </>
+  )
+}
+
+export default ProductTemplate
