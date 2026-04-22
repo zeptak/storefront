e059f784c6019aa331d889d9b96d97fe8b5bda99
zeptak <33266931+zeptak@users.noreply.github.com>
2026-04-22 12:21:14 +0200

    Initialize demo-branch

---
+++ src/lib/config.ts
@@ -0,0 +1,14 @@
+import Medusa from "@medusajs/js-sdk"
+
+// Defaults to standard port for Medusa server
+let MEDUSA_BACKEND_URL = "http://localhost:9000"
+
+if (process.env.MEDUSA_BACKEND_URL) {
+  MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL
+}
+
+export const sdk = new Medusa({
+  baseUrl: MEDUSA_BACKEND_URL,
+  debug: process.env.NODE_ENV === "development",
+  publishableKey: "pk_f696c9fcea8036abecccd3739c3256ebb2151fa7cbed0ff2738c6070c4d374ef",
+})
